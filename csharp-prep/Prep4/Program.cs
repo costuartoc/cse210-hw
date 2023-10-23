5ee91ea21788c26d98624cbfda481826e3c97dff
Stuart Olmstead <olm23001@byui.edu>
2023-10-23 14:59:02 -0700

    Finished prep project 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers type 0 when finished.");
+
+        int condition = -1;
+
+        while (condition == -1)
+        {
+            Console.Write("Enter Number (0 to quit): ");
+            string inNumber = Console.ReadLine();
+            int number = int.Parse(inNumber);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+            else{
+                condition = 0;
+            }
+        }
+        
+        int sum = 0;
+        int largest = 0;
+
+        foreach ( int number in numbers)
+        {
+            sum = sum + number;
+            
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+    
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"Thelargest number is: {largest}");
     }
 }
