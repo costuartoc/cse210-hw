4f500ce0b6e316f8ae106f8f9f210646fe17c1ad
Stuart Olmstead <olm23001@byui.edu>
2023-10-23 14:05:21 -0700

    Completed Prep 2 Project

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradePercent = Console.ReadLine();
+        int gradeNum = int.Parse(gradePercent);
+
+        string letter = "";
+        
+        if (gradeNum >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeNum >= 80 && gradeNum < 90)
+        {
+            letter = "B";
+        }
+        else if (gradeNum >= 70 && gradeNum < 80)
+        {
+            letter = "C";
+        }
+        else if (gradeNum >= 60 && gradeNum < 70)
+        {
+            letter = "D";
+        }
+        else if (gradeNum < 60)
+        {
+            letter = "F";
+        }
+        
+        if (letter == "A" || letter == "F")
+        {
+            Console.WriteLine($"You got an {letter}");
+        }
+        else 
+        {
+            Console.WriteLine($"You got a {letter}");
+        }
+
+        if (letter == "A" || letter == "B" || letter == "C")
+        {
+            Console.WriteLine("Congratulations you passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry you didn't pass the course, study hard and i'm sure you'll get it next time!");
+        }
     }
+
 }
