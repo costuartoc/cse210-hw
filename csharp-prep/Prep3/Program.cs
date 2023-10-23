542a1b5c527d875aa7b8028e985cd088b086be79
Stuart Olmstead <olm23001@byui.edu>
2023-10-23 14:30:03 -0700

    Finished prep project 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("Can you guess the magic number? ");
+        
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        
+        string condition = "no";
+
+        while (condition == "no")
+        {
+            Console.Write("What is your guess? ");
+            string inGuess = Console.ReadLine();
+            int guess = int.Parse(inGuess);
+            
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                condition = "yes";
+            }
+        }
     }
 }
