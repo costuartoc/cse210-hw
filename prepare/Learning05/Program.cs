7957d9b3749040aa66ffcf555eba78cad29dfacd
Stuart Olmstead <olm23001@byui.edu>
2023-11-28 17:37:03 -0700

    Finished L05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        
+        Square s1 = new Square("Red", 3);
+        shapes.Add(s1);
+
+        Rectangle r1 = new Rectangle("Blue", 4, 5);
+        shapes.Add(r1);
+
+        Circle c1 = new Circle("Green", 6);
+        shapes.Add(c1);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
