644b7953461ec2da374f9cca72d0bd3ff796c530
Stuart Olmstead <olm23001@byui.edu>
2023-12-02 14:42:10 -0700

    Added a comment

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,10 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Added Console.Clear functions to make navigating through the menu and different options cleaner
         GoalManager goalManager1 = new GoalManager();
         
         goalManager1.Start();
-
-        
+ 
     }
 }
